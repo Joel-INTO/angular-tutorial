dfd7965a56cdd8ec2642b9fe4f80a056a2de3790
Joel <joel.balmer@intoglobal.com>
2019-02-05 16:26:14 +0000

    final lazy loading chapter dome

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -13,7 +13,6 @@ import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 /* Feature Modules */
-import { ProductModule } from './products/product.module';
 import { UserModule } from './user/user.module';
 import { MessageModule } from './messages/message.module';
 
@@ -24,7 +23,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     HttpClientModule,
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
-    ProductModule,
     UserModule,
     MessageModule,
     AppRoutingModule,
